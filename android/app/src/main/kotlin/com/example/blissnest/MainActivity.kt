0d7aecd5a7bd463fae9b9ff10920710553177b59
ro35ert <trebrorobertsbob@gmail.com>
2024-10-05 00:19:30 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/blissnest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blissnest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color peachColor = Color(0xFFFFCBA4); // Peach color
+const Color orangeColor = Color(0xFFFF8C00); // Orange color
+const Color backgroundColor = Color(0xFFF5F5F5);  // Light background color
