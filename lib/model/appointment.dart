4466ba9a14607d076391d64aa9edae5c8cd72c6b
ro35ert <trebrorobertsbob@gmail.com>
2024-10-15 23:15:20 +0200

    added appointment

---
+++ lib/model/appointment.dart
@@ -0,0 +1,45 @@
+class Appointment {
+  final String id;
+  final String title;
+  final DateTime date;
+  final String status;
+  final String description;
+  final int doctorId;
+  final int patientId;
+
+  Appointment({
+    required this.id,
+    required this.title,
+    required this.date,
+    required this.status,
+    required this.description,
+    required this.doctorId,
+    required this.patientId,
+  });
+
+  // Factory constructor to create an instance of Appointment from a JSON map
+  factory Appointment.fromJson(Map<String, dynamic> json) {
+    return Appointment(
+      id: json['id'],
+      title: json['title'],
+      date: DateTime.parse(json['date']),
+      status: json['status'],
+      description: json['description'],
+      doctorId: json['doctorId'],
+      patientId: json['patientId'],
+    );
+  }
+
+  // Method to convert an Appointment instance to a JSON map
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'date': date.toIso8601String(),
+      'status': status,
+      'description': description,
+      'doctorId': doctorId,
+      'patientId': patientId,
+    };
+  }
+}
