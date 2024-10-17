741a6f1b2ab6547660bbbb2b655da42aed3d5d1b
ro35ert <trebrorobertsbob@gmail.com>
2024-10-17 19:33:32 +0200

    updated

--- lib/presentation/screens/appointment.dart
+++ lib/presentation/screens/appointment.dart
@@ -286,7 +286,7 @@ class _AppointmentsTabState extends State<AppointmentsTab> {
                     ),
                     TextButton(
                       onPressed: () {
-                        Navigator.of(context).pop(); // Close the dialog
+                        Navigator.of(context).pop();
                       },
                       child: const Text('Cancel'),
                     ),
